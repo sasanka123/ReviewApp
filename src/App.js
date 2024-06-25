a5fc60acdbafa400e0b59bfaad0f9b12d0de8f00
Sasanka Herath <sasanka.herath@qbe.com>
2024-06-26 11:36:24 +1200

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { v4 as uuiidv4 } from "uuid";
+import { useState } from "react";
+import Header from "./components/Header";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData";
+import FeedbackStats from "./components/FeedbackStats";
+import FeedbackForm from "./components/FeedbackForm";
 
 function App() {
+  const [feedback, setFeedback] = useState(FeedbackData);
+  const deleteFeedback = (id) => {
+    if (window.confirm("Are you sure you want to delete?")) {
+      setFeedback(feedback.filter((item) => item.id !== id));
+    }
+  };
+
+  const addFeedback = (newFeedback) => {
+    newFeedback.id = uuiidv4();
+    setFeedback([newFeedback, ...feedback]); //...>>spread operator >> get list of all the current feedback items
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Header />
+      <div className="container">
+        <FeedbackForm handleAdd={addFeedback} />
+        <FeedbackStats feedback={feedback} />
+        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+      </div>
+    </>
   );
 }
-
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types'
+
+
+function Header({text}) {
+    const headerStyle = {backgroundColor:'rgba(0,0,0,0.4)', color:'#ff6a95'}
+  return (
+   <header style={headerStyle}>
+    <div className="container">
+       <h2>{text}</h2>
+    </div>
+   </header>
+  )
+}
+Header.defaultProps={
+    text:'Feedback UI'
+}
+Header.propTypes = {
+text:PropTypes.string.isRequired
+}
+export default Header
+
